fa1b564660e5fccfa5973748db3854c9c2697c1f
Amogh M Cheluvaraj <amogh.mcheluvaraj@fnf.com>
2024-06-28 16:08:02 +0530

    update pr04

--- Pr04RuntimePolymorphism.cs
+++ Pr04RuntimePolymorphism.cs
@@ -4,11 +4,22 @@ class Base {
     public virtual void TestFunc() {
         Console.WriteLine("Base Class called!");
     }
+    public void NonOverriden() {
+        Console.WriteLine("This is from Base");
+    }
 }
 
 class Derived : Base {
     public override void TestFunc() {
         Console.WriteLine("Derived Class called!");
+    }
+
+    public void SecondTestFunc() {
+        Console.WriteLine("Second Test Function!");
+    }
+
+    public void NonOverriden() {
+        Console.WriteLine("This is from Derived");
     }
 }
 
@@ -30,6 +41,17 @@ class Test {
     static void Main() {
         var choice = MyConsole.GetString("Base (B) or Derived? (D)?");
         var obj = Factory.CreateClass(choice.ToUpper());
+        try {
+            checked {
+                var temp = (Derived) obj;
+                temp.SecondTestFunc();
+                temp.NonOverriden();
+            }
+        }
+        catch (Exception ex) {
+            Console.WriteLine(ex.Message);
+        }
         obj.TestFunc();
+        obj.NonOverriden();
     }
 }
