1c198541a270a9478612d1e44e6ff90cea7492dd
Amogh M Cheluvaraj <amogh.mcheluvaraj@fnf.com>
2024-07-01 15:59:19 +0530

    add assessment_1

---
+++ Assessment_1.cs
@@ -0,0 +1,70 @@
+using System;
+
+class Test {
+    static void Main() {
+        // 1st question
+        StringEncoderUnitTests();
+        // StringEncoderUserTest();
+
+        // 2nd question
+        // todo
+    }
+
+    static void StringEncoderUserTest() {
+        string msg = "The quick and brown fox jumps over the Lazy Dog";
+        StringEncoder strEncoder = new StringEncoder(msg);
+        
+        while (true) {
+            try {
+                String userInput = MyConsole.GetString("Enter a string to encode:");
+                Console.WriteLine(strEncoder.GetEncodedMessage(userInput));
+            }
+            catch (NullReferenceException ex) {
+                Console.WriteLine(ex.Message);
+            }
+            catch (ArgumentException ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+
+    static void StringEncoderUnitTests() {
+        string msg = "The quick and brown fox jumps over the Lazy Dog";
+        StringEncoder strEncoder = new StringEncoder(msg);
+
+        // null test
+        try {
+            Console.WriteLine(strEncoder.GetEncodedMessage(null));
+        }
+        catch (NullReferenceException ex) {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine(ex.Message);
+        }
+
+        // empty string test
+        try {
+            Console.WriteLine(strEncoder.GetEncodedMessage(""));
+        }
+        catch (NullReferenceException ex) {
+            Console.WriteLine(ex.Message);
+        }
+        catch (ArgumentException ex) {
+            Console.WriteLine(ex.Message);
+        }
+
+        // additional tests
+        Console.WriteLine(strEncoder.GetEncodedMessage("bangalore"));
+        Console.WriteLine(strEncoder.GetEncodedMessage("Global Village"));
+        Console.WriteLine(strEncoder.GetEncodedMessage("TTG"));
+        Console.WriteLine(strEncoder.GetEncodedMessage("mindtree"));
+        Console.WriteLine(strEncoder.GetEncodedMessage("West Campus"));
+        // starting with space
+        Console.WriteLine(strEncoder.GetEncodedMessage(" bangalore"));
+        // ending with space
+        Console.WriteLine(strEncoder.GetEncodedMessage("bangalore "));
+        // empty with space
+        Console.WriteLine(strEncoder.GetEncodedMessage("  "));
+    }
+}
--- StringEncoder.cs
+++ StringEncoder.cs
@@ -21,13 +21,14 @@ public class StringEncoder {
             if (!charToInt.ContainsKey(item) && !item.Equals(' '))
             {
                 charToInt[item] = string.Format("{0}{1}", wordCount, letterCount);
-                Console.WriteLine("{0} {1}", item, charToInt[item]);
             }
         }
     }
 
     public string GetEncodedMessage(string message) {
         string encoded = "";
+        var check = 0;
+        message = message.ToLower();
         if (message == (null)) {
             throw new NullReferenceException();
         }
@@ -37,29 +38,15 @@ public class StringEncoder {
         foreach (var item in message) {
             if (item.Equals(' ')) {
                 encoded += "-";
+                check = 1;
                 continue;
             }
-            if (!encoded.Equals("")) {
+            if (!encoded.Equals("") && check == 0) {
                 encoded += ",";
             }
+            check = 0;
             encoded += charToInt[item];
         }
         return encoded;
-    }
-
-    static void Main() {
-        string msg = "The quick and brown fox jumps over the Lazy Dog";
-        StringEncoder strEncoder = new StringEncoder(msg);
-        try {
-            Console.WriteLine(strEncoder.GetEncodedMessage("bangalore"));
-            Console.WriteLine(strEncoder.GetEncodedMessage(null));
-            Console.WriteLine(strEncoder.GetEncodedMessage(""));
-        }
-        catch (NullReferenceException ex) {
-            Console.WriteLine(ex.Message);
-        }
-        catch (ArgumentException ex) {
-            Console.WriteLine(ex.Message);
-        }
     }
 }
