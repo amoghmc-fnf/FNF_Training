8b1831244ec7061e3e650a939890d77c549b277f
Amogh M Cheluvaraj <amogh.mcheluvaraj@fnf.com>
2024-07-09 09:47:36 +0530

    add JS

---
+++ Web technologies/JS/Ex01Basics.js
@@ -0,0 +1,10 @@
+alert("Happy to learn JS")
+let res = prompt("what do u want to do today?")
+alert(res)
+
+res = confirm("do u really want to learn JS?")
+if (res)
+    alert("lets go ahead to learn")
+else
+    alert("you missed a great opportunity to learn a beautiful language")
+
