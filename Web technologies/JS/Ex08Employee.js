cf648aa37298cf69259950eed96f2af0b2456540
Amogh M Cheluvaraj <amogh.mcheluvaraj@fnf.com>
2024-07-10 16:47:39 +0530

    add ex09

--- Web technologies/JS/Ex08Employee.js
+++ Web technologies/JS/Ex08Employee.js
@@ -7,3 +7,29 @@ class Employee {
     }
     display = () => `${this.name} is from ${this.address} and has raised the bill amount of Rs.${this.salary}`
 }
+
+class EmployeeRepo {
+    data = [new Employee(1, "phaniraj", "bangalore", 56000),
+        new Employee(2, "ram", "mysore", 50000),
+        new Employee(3, "vinod", "bangalore", 76000),
+        new Employee(4, "shyam", "hyderabad", 45000),
+    ];
+
+    addNewEmployee = (emp) => [...this.data, emp]
+
+    deleteEmployee = (id) => {
+        let index = this.data.findIndex((e) => e.empId == id);
+        this.data.splice(index, 1)
+    }
+
+    getAllEmployees = () => this.data
+
+    updateEmployee = (id, emp) => {
+        let index = this.data.findIndex((e) => e.empId == id)
+        this.data.splice(index, 1, emp)
+    }
+}
+
+const hide = (...elements) => [...elements].forEach(e => (e.style.display = 'none'))
+
+const show = (id) => document.getElementById(id).style.display = 'block'
